13295bc76ab2940247b5509d38f0afb0c96c2444
Yacine Boudebouz <yacineboudebbouz7@gmail.com>
2023-12-24 20:19:02 +0100

    1.0

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,7 +3,6 @@ import 'package:express_shop/src/routing/app_router.dart';
 import 'package:express_shop/src/theme/pallete.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
--- lib/src/theme/pallete.dart
+++ lib/src/theme/pallete.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
 
